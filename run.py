007aefbdb6096e9f8d4da6d4c23f5b59e39bc408
Ray$$$ <souvikray999@gmail.com>
2018-01-15 21:38:50 +0530

    fixed 'db' not found error

--- run.py
+++ run.py
@@ -1,9 +1,10 @@
 from app import app
 from db import db
 
+# enable sqlalchemy to use the flask app
+db.init_app(app)
 # create a database before flask starts accepting requests
 @app.before_first_request
 def create_tables():
     db.create_all()
-# enable sqlalchemy to use the flask app
-db.init_app(app)
+
